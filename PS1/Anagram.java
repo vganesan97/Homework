b1381545f597e0b8d1a8ffa18c2947102e92601b
Aravind Sundaresan <sundrsn2@illinois.edu>
2015-09-01 16:27:55 -0500

    rename

---
+++ PS1/Anagram.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.io.*;
+
+/*
+************************************************
+  ____ ____    _  ___   __   
+ / ___/ ___|  / |/ _ \ / /_  
+| |   \___ \  | | (_) | '_ \ 
+| |___ ___) | | |\__, | (_) |
+ \____|____/  |_|  /_/ \___/ 
+                             
+
+Problem set 1
+
+
+Question 1
+
+A common problem in computer science is finding patterns within data. 
+This problem will simulate that in a way that is easy to see what is happening.
+
+A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward.
+Allowances may be made for adjustments to capital letters, punctuation, and word dividers.
+
+an anagram is a word, phrase, or name formed by rearranging the letters of another.
+
+
+Given a String S, determine if it is an anagram of a palindrome. 
+Return true if the String is an anagram of a palindrome, and false otherwise. 
+For example, the String “oatrtro” will return true (rotator), while the String “false” will return false.
+
+
+PLEASE LOOK AT PS1.txt FOR MORE DETAILS!!!
+
+************************************************
+
+*/
+
+public class Anagram {
+
+	public static boolean anagram(String input) {
+		//YOUR CODE HERE
+		return false;
+	}
+
+
+
+	public static void main(String[] args) {
+		File file = new File("Anagram.txt");
+		try {
+			Scanner scan = new Scanner(file);
+			int numberOfCases = scan.nextInt();
+			for(int i = 0; i < numberOfCases; i++) {
+				String input = scan.next();
+				System.out.println(anagram(input));
+			}
+			scan.close();
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ PS1/Stairs.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+/*
+************************************************
+  ____ ____    _  ___   __   
+ / ___/ ___|  / |/ _ \ / /_  
+| |   \___ \  | | (_) | '_ \ 
+| |___ ___) | | |\__, | (_) |
+ \____|____/  |_|  /_/ \___/ 
+                             
+
+Problem set 1
+
+
+Question 3
+
+Given an integer input N, print a staircase out of height N. For example, given the input 4, print out the following staircase:
+
+    #
+   ##
+  ###
+ ####
+#####
+
+
+PLEASE LOOK AT PS1.txt FOR MORE DETAILS!!!
+
+************************************************
+
+*/
+public class Stairs {
+
+
+	public static void stairs(int input) {
+		//YOUR CODE HERE
+		return;
+	}
+
+
+	public static void main(String[] args) {
+		File file = new File("Stairs.txt");
+		try {
+			Scanner scan = new Scanner(file);
+			int numberOfCases = scan.nextInt();
+			for(int i = 0; i < numberOfCases; i++) {
+				int height = scan.nextInt();
+				stairs(height);
+			}
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
