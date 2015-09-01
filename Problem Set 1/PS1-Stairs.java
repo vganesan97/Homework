a9acc31f5536e8a85a088fd2ae558891b0db5ceb
Aravind Sundaresan <sundrsn2@illinois.edu>
2015-09-01 15:41:34 -0500

    initial pset 1

---
+++ Problem Set 1/PS1-Stairs.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+/*
+************************************************
+  ____ ____    _  ___   __   
+ / ___/ ___|  / |/ _ \ / /_  
+| |   \___ \  | | (_) | '_ \ 
+| |___ ___) | | |\__, | (_) |
+ \____|____/  |_|  /_/ \___/ 
+                             
+
+Problem set 1
+
+
+Question 3
+
+Given an integer input N, print a staircase out of height N. For example, given the input 4, print out the following staircase:
+
+    #
+   ##
+  ###
+ ####
+#####
+
+*/
+
+public static void stairs() {
+	Scanner scan = new Scanner(System.in);
+	int input = scan.nextInt();
+	//YOUR CODE HERE
+	return;
+}
