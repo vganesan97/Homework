00ae547e34dcadf2f8ef8b3eed3f28942e90551f
Rohan Mathur <rohanmathur34@gmail.com>
2015-09-15 17:46:11 -0500

    Update problem 4 with one more issue
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

--- PS2/problem4.java
+++ PS2/problem4.java
@@ -15,27 +15,35 @@ This program causes something called an infinite loop. This means that the
 program never ends because of a logical error.
 
 Why doesn't this program stop running?
-There are two errors.
+There are three issues to address.
 ************************************************
 */
 
-public class Problem4 {
+class Problem4 {
     public static void main(String[] args) {
         printFirstSentence();
         printSecondSentence();
+        printThirdSentence();
     }
 
-    public void printFirstSentence() {
+    public static void printFirstSentence() {
         for(int i = 5; i > 0; i++) {
             System.out.println("This sentence should print 5 times.");
         }
     }
 
-    public void printSecondSentence() {
+    public static void printSecondSentence() {
         int i = 3;
         while(i > 0) {
             System.out.println("This sentence should print 3 times.");
             i++;
+        }
+    }
+
+    public static void printThirdSentence() {
+        for(int amount = 0; amount < 10; amount++) {
+            System.out.println("This sentence should print 5 times")
+            amount--;
         }
     }
 }
