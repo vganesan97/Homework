3a81f677a7d720bbdb6392ae79b3349a69fd9cc9
Rohan Mathur <rohanmathur34@gmail.com>
2015-09-15 18:01:20 -0500

    Add problem 3
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

---
+++ PS2/problem3.py
@@ -0,0 +1,22 @@
+"""
+************************************************
+  ____ ____    _  ___   __
+ / ___/ ___|  / |/ _ \ / /_
+| |   \___ \  | | (_) | '_ \
+| |___ ___) | | |\__, | (_) |
+ \____|____/  |_|  /_/ \___/
+
+Problem Set 1
+Problem 3
+
+Find the python bug!
+Should print 'CS196' when completed
+************************************************
+"""
+
+def printStatement(foo):
+    length = foo.length;
+    for i in xrange(length+1):
+        System.out.println(i[foo]);
+
+printStatement(['CS','1','9','6'])
